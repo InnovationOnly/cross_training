9d692204378e4fff1e6e95f876cf4f67f9445d3a
freddyjeanty <jeantyfreddy.j@gmail.com>
2019-01-05 23:29:35 +0100

    [ DEV ] Init QML

---
+++ QML_C++/Treaning/chrono.cpp
@@ -0,0 +1,11 @@
+#include "chrono.h"
+
+Chrono::Chrono(QObject *parent) : QObject(parent)
+{
+
+}
+
+void Chrono::isButtonClick(bool isClick){
+
+}
+
---
+++ QML_C++/Treaning/chrono.h
@@ -0,0 +1,21 @@
+#ifndef CHRONO_H
+#define CHRONO_H
+
+#include <QObject>
+
+class Chrono : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Chrono(QObject *parent = nullptr);
+
+signals:
+
+    void isButtonClick(bool isClick);
+
+public slots:
+
+    void startChrono(bool isClick);
+};
+
+#endif // CHRONO_H
